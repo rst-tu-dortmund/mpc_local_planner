3a0d35dde8a522b405cd318d5fccf9038cdeb70d
Christoph Rösmann <christoph.roesmann@tu-dortmund.de>
2020-03-03 11:06:24 +0100

    replaced non-ASCII characters in python script

--- mpc_local_planner/scripts/plot_optimal_control_results.py
+++ mpc_local_planner/scripts/plot_optimal_control_results.py
@@ -3,7 +3,7 @@
 #
 #  Software License Agreement
 #
-#  Copyright (c) 2020, Christoph Rösmann, All rights reserved.
+#  Copyright (c) 2020, Christoph Roesmann, All rights reserved.
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-#  Authors: Christoph Rösmann
+#  Authors: Christoph Roesmann
 
 import rospy, math
 from mpc_local_planner_msgs.msg import OptimalControlResult
